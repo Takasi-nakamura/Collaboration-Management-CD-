28c89e8d0aa21580db8c355216d6eca7fcb2f01a
Takasi-nakamura <haruto.yomogida@gmail.com>
2026-09-02 12:25:43 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 /* 開発中はここにFirebaseのWebアプリ設定を直接入れてください。公開前に.env方式へ移行できます。 */
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBJSyPxVJDWezDvLhYWJ80TWFv441IeA8A",
+  authDomain: "toolbox-9ae26.firebaseapp.com",
+  projectId: "toolbox-9ae26",
+  storageBucket: "toolbox-9ae26.firebasestorage.app",
+  messagingSenderId: "102342085744",
+  appId: "1:102342085744:web:f92a560d27432e197878b5"
 };
+
 
 window.CM_FIREBASE_CONFIGURED = Boolean(firebaseConfig.apiKey && firebaseConfig.projectId && firebaseConfig.appId);
