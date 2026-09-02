cddbd18ceda29185b081ab79e4f7369dbd040f1d
Takasi-nakamura <haruto.yomogida@gmail.com>
2026-09-02 12:30:10 +0900

    Expose Firebase configuration to the app

--- firebase-config.js
+++ firebase-config.js
@@ -7,8 +7,5 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-window.CM_FIREBASE_CONFIGURED = Boolean(
-  firebaseConfig.apiKey &&
-  firebaseConfig.projectId &&
-  firebaseConfig.appId
-);
+window.CM_FIREBASE_CONFIGURED = Boolean(firebaseConfig.apiKey && firebaseConfig.projectId && firebaseConfig.appId);
+window.CM_FIREBASE_CONFIG = firebaseConfig;
